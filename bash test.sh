14d7068d8bd416d7e86508f88a9fc5a8cdb7b141
Bajirao Shrihari Todkar <140590960+BAJITODKAR@users.noreply.github.com>
2025-08-20 23:45:03 +0530

    Create bash test.sh

---
+++ bash test.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Simple script to check Jenkins status and start if stopped
+
+SERVICE="jenkins"
+
+# Check status
+if systemctl is-active --quiet $SERVICE
+then
+    echo "✅ Jenkins is already running."
+else
+    echo "⚠️ Jenkins is not running. Starting it now..."
+    sudo systemctl start $SERVICE
+    sleep 2
+    systemctl status $SERVICE
+fi
